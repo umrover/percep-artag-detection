ff2044754c1790926d25648cac3acd8add31b39f
vidyasilai <vsilai@DESKTOP-BDUJ2T8.localdomain>
2021-03-14 14:18:01 -0400

    Drawing circles on corners

--- ar.cpp
+++ ar.cpp
@@ -34,11 +34,11 @@ void pipeline(Mat src) {
     src.copyTo(src_color_filtered, mask1);
 
     //convert image to gray
-    cvtColor( src_color_filtered, src_gray, COLOR_BGR2GRAY );
+    cvtColor(src_color_filtered, src_gray, COLOR_BGR2GRAY );
 
     //use canny edge detection to find edges in the image
     Mat canny_output;
-    Canny( src_gray, canny_output, 300, 900 );
+    Canny(src_gray, canny_output, 300, 900);
     
     //use findContours to find contours based on canny edge detection
     vector< vector<Point> > contours;
@@ -47,10 +47,10 @@ void pipeline(Mat src) {
 
     //filter contours based on bounding rectangle aspect ratio; must be between .75 and 2
     vector< vector<Point> > contours_accepted;
-    vector<Rect> boundRect(contours.size());
-    vector<Rect> accepted_rect_aspect(contours.size());
+    /*vector<Rect> boundRect(contours.size());
+    vector<Rect> accepted_rect_aspect(contours.size()); */
 
-    for(size_t i = 0; i < contours.size(); ++i) {
+    /*for(size_t i = 0; i < contours.size(); ++i) {
         boundRect.at(i) =  boundingRect(contours.at(i));
         if((double)(boundRect.at(i).width)/(double)(boundRect.at(i).height) < 2 && 
         (double)(boundRect.at(i).width)/(double)(boundRect.at(i).height) > .5 && 
@@ -58,12 +58,9 @@ void pipeline(Mat src) {
             contours_accepted.push_back(contours.at(i));
             accepted_rect_aspect.push_back(boundRect.at(i));
         }
-    }
-
-    
-    
-    
-    //filter bounding boxes based on whether or not they overlap
+    } */
+ 
+    //Filter bounding boxes based on whether or not they overlap
     // vector<Rect> accepted_rect(contours.size());
     // Rect temp;
 
@@ -82,34 +79,62 @@ void pipeline(Mat src) {
     //draw bounding rects
     Mat bounding_mat = Mat::zeros(canny_output.size(), CV_8UC3);
 
-    for(int i = 0; i < accepted_rect_aspect.size(); i++) {
-        rectangle(src, accepted_rect_aspect.at(i), Scalar(0,0,255),2);
-    }
+    //for(int i = 0; i < accepted_rect_aspect.size(); i++) {
+        //rectangle(src, accepted_rect_aspect.at(i), Scalar(0,0,255),2);
+    //}
 
     //draw contours
     Mat contour_mat = Mat::zeros(canny_output.size(), CV_8UC3);
 
-    for(int i = 0; i < contours_accepted.size(); i++) {
-        drawContours( src, contours_accepted, (int)i, Scalar(94,223,95), 2, LINE_8, hierarchy, 0 );
+    // Detecting corners
+    cornerHarris(src_gray, bounding_mat, 7, 5, 0.05, BORDER_DEFAULT );
+
+    // Normalizing
+    Mat bounding_mat_norm;
+    Mat bounding_mat_scaled;
+    normalize(bounding_mat, bounding_mat_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
+    convertScaleAbs(bounding_mat_norm, bounding_mat_scaled);
+
+    // Drawing a circle around corners
+    int circleCounter;
+
+    for( int j = 0; j < bounding_mat_norm.rows ; j++ )
+    { for( int i = 0; i < bounding_mat_norm.cols; i++ )
+    {
+        if( (int) bounding_mat_norm.at<float>(j,i) > thresh )
+        {
+           circle(bounding_mat_scaled, Point( i, j ), 5,  Scalar(0), 2, 8, 0);
+           ++circleCounter;
+        }
+    }
     }
 
+
+    //for(int i = 0; i < contours_accepted.size(); i++) {
+        //drawContours(src, contours_accepted, (int)i, Scalar(94,223,95), 2, LINE_8, hierarchy, 0 );
+    //}
+
     //display results
-    namedWindow( "Source w/ Bounding Rects", WINDOW_AUTOSIZE );
-    namedWindow("hsv", WINDOW_AUTOSIZE);
- 	//namedWindow( "Contours", WINDOW_AUTOSIZE );
+    //namedWindow( "Source w/ Bounding Rects", WINDOW_AUTOSIZE );
+    //namedWindow("hsv", WINDOW_AUTOSIZE);
+ 	//namedWindow("Contours", WINDOW_AUTOSIZE);
     
- 	imshow("Source w/ Bounding Rects", src);
-    imshow("hsv", src_color_filtered);
+ 	//imshow("Source w/ Bounding Rects", src);
+    //imshow("hsv", src_color_filtered);
  	//imshow("Contours", contour_mat);
     
- 	waitKey(0);
+ 	// Showing the result
+    namedWindow("corners_window", CV_WINDOW_AUTOSIZE);
+    imshow("corners_window", bounding_mat_scaled);
+
+    waitKey(0);
 }
 
 int main( int argc, char** argv )
 {
     //get image name from command line for testing
     /*CommandLineParser parser( argc, argv, "{@input | input image}" );
-    Mat src = imread( samples::findFile( parser.get<String>( "@input" ) ) );
+    Mat src = imread(samples::findFile( parser.get<String>( "@input" ) ) );
     if( src.empty() )
     {
       cout << "Could not open or find the image!\n" << endl;
